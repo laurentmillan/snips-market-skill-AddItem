4eed1b87e212ab7408594b9ced74fa91c64724e9
Laurent Millan <laurent.millan@gmail.com>
2018-05-15 00:15:51 +0200

    Ajout de fonctions mock up

--- action-shivan-AddItem.js
+++ action-shivan-AddItem.js
@@ -14,10 +14,56 @@ client.on('connect', function () {
 })
 
 
+var callback_AddItem = function(err, resp, body){
+	// Ajoute x à la liste y
+	console.log("*****\nAddItem", body);
+}
+
+var callback_SearchItem = function(err, resp, body){
+	// Est-ce qu'il y a p dans la liste l ?
+	console.log("*****\nSearchItem", body);
+}
+
+var callback_ListItems = function(err, resp, body){
+	// Donne moi la liste l
+	console.log("*****\nListItems", body);
+}
+
+var callback_NumberItems = function(err, resp, body){
+	// Combien il y a de p dans la liste l
+	console.log("*****\nNumberItems", body);
+}
+
+var lastIntentDetails;
+
 client.on('message', function (topic, message) {
   // message is Buffer
   console.log(topic)
   let intentDetails = JSON.parse(message.toString());
   console.log(intentDetails);
   lastIntentDetails = intentDetails;
-});
+
+  switch(topic){
+	case "hermes/hotword/default/detected":
+		player.play('bip.wav', function(err){
+		  if (err) throw err
+		})
+		break;
+	case "hermes/intent/shivan:AddItem":
+    callback_AddItem(req, res, body)
+		//request.post(options, callback_AddItem);
+		break;
+	case "hermes/intent/shivan:SearchItem":
+    callback_SearchItem(req, res, body)
+		//request.post(options, callback_SearchItem);
+		break;
+	case "hermes/intent/shivan:ListItems":
+    callback_ListItems(req, res, body)
+		//request.post(options, callback_ListItems);
+		break;
+	case "hermes/intent/shivan:NumberItems":
+    callback_NumberItems(req, res, body)
+    //request.post(options, callback_NumberItems);
+		break;
+  }
+})
